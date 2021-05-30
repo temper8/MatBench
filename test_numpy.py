94759b65a573b0510842a4f7bb476f9aa02d5df8
Alex <temper8@gmail.com>
2021-05-30 20:44:46 +0300

    simple test for matrix mult

--- test_numpy.py
+++ test_numpy.py
@@ -1,17 +1,19 @@
 import numpy as np 
+from benchmark import exec_time
 
-
+@exec_time()
 def create_mat(n):
 	A = np.random.sample((n,n))
 	B = np.random.sample((n,n))
 	return (A,B)
 
+@exec_time()
 def mult_mat(A,B):
 	return np.dot(A,B)
 
+n = 15000
+print(f"numpy mult test N={n}")
 
-print("numpy test")
-
-(a,b) = create_mat(3)
+(a,b) = create_mat(n)
 c = mult_mat(a,b)
-print(c)
+#print(c)
