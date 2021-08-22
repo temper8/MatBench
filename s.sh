a94fc98e4f500f0f06348c2b9c43aa10278c0a7d
Alex <temper8@gmail.com>
2021-08-23 01:11:31 +0300

    Delete s.sh

--- s.sh
+++
@@ -1,1 +0,0 @@
-ifort solve.f90 -I${MKLROOT}/include/intel64/ilp64 -i8  -I"${MKLROOT}/include" ${MKLROOT}/lib/intel64/libmkl_blas95_ilp64.a ${MKLROOT}/lib/intel64/libmkl_lapack95_ilp64.a -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -liomp5 -lpthread -lm -ldl
