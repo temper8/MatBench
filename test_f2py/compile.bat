f79b84beba070f7e323915a2e0be4f18b4405f12
Alex <temper8@gmail.com>
2021-09-01 03:00:30 +0300

    Update compile.bat

--- test_f2py/compile.bat
+++ test_f2py/compile.bat
@@ -1,3 +1,5 @@
-call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat"
-echo "test"
-python -m numpy.f2py -c fib1.f90 -m fib1
+@echo off
+call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" 
+python -m numpy.f2py -c fib1.f90 -m fib1  > compile.log
+@echo on
+echo compile fib1.f90 
